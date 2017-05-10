562c83b2f0012ce06f0a2c9821951e52c6a41bce
Pedro Ontiveros <pedro.ontiveros@globant.com>
2017-05-10 12:24:47 -0500

    Adding CleaItunesCore Framework

---
+++ CleanItunesCore/CleanItunesCore/CleanItunesCore.h
@@ -0,0 +1,19 @@
+//
+//  CleanItunesCore.h
+//  CleanItunesCore
+//
+//  Created by Vangeli Ontiveros on 5/10/17.
+//  Copyright © 2017 WDPRO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CleanItunesCore.
+FOUNDATION_EXPORT double CleanItunesCoreVersionNumber;
+
+//! Project version string for CleanItunesCore.
+FOUNDATION_EXPORT const unsigned char CleanItunesCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CleanItunesCore/PublicHeader.h>
+
+
--- CleanItunesSearch/ViewController.swift
+++ CleanItunesSearch/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import CleanItunesCore
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    let request = ItunesSearchRequest(term: "LOL")
+
   }
 
   override func didReceiveMemoryWarning() {
