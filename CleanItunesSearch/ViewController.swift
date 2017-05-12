c0f4825585c3fa77c044dfd3d4d15e433d73be44
Pedro Ontiveros <pedro.ontiveros@globant.com>
2017-05-12 12:39:01 -0500

    Added Realm Framework

--- CleanItunesSearch/ViewController.swift
+++ CleanItunesSearch/ViewController.swift
@@ -13,9 +13,7 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-    let request = ItunesSearchRequest(term: "LOL")
-
+    // Do any additional setup after loading the view, typically from a nib
   }
 
   override func didReceiveMemoryWarning() {
